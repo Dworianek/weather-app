ae07263ef28de928ebc00c59284bb23d4a4dca7b
Adrian Dworak <adrian.dworak97@gmail.com>
2023-02-16 21:03:58 +0100

    main commit 16.02

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [weather, setWeather] = useState({});
+
+  useEffect(() => {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=Zawiercie&units=metric&appid=2ee1aa6d12ead49998035fd8f3c426f5"
+    )
+      .then((ressposne) => ressposne.json())
+      .then((data) => {
+        setWeather(data);
+        console.log(data);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="weatherApp">
+      <div className="cityForm">
+        <input type="text" name="" id="Your City" />
+        <button>Wyszukaj</button>
+      </div>
+      <p>Nazwa miasta : {`Poręba`}</p>
+      <p>Temperatura : {`2 °C`} </p>
     </div>
   );
 }
